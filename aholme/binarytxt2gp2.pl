a5d36d11999bc6d433fbc4582b4c8008e1b234ba
Matija Nalis <mnalis-android@voyager.hr>
2015-01-05 02:27:52 +0100

    uh, those are raw 30-bit values, not our 32-bit extended with zeros. compensate.

--- aholme/binarytxt2gp2.pl
+++ aholme/binarytxt2gp2.pl
@@ -8,9 +8,9 @@ sub b2h {
     my $bin = shift;
     my $hex = '';
     while ($bin) {
-        my $cut = substr($bin,0,8,'');
+        my $cut = substr($bin,0,30,'');	# gps uses 30bit words, not 32bit, so we pad MSB with '00'
         #say "cut=$cut";
-        $hex .= sprintf('%02X ', oct("0b$cut"));
+        $hex .= sprintf('%08X', oct("0b$cut"));
     }
     return $hex;
 }
@@ -22,5 +22,6 @@ my $count=0;
 while (<$in>) {
     chomp;
     my $hex = b2h($_);
+    $hex =~ s/(..)/$1 /g;
     printf "00/00/0008 00:00:%02d.%03d (0) 85 01 01 00 FF 00 00 00 00 00 FF $hex\n", 0, $count++;
 }
